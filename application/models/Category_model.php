b6d24deaa7d81a3c488f76af0b9fa0d7a85b34e0
daesca <reickchozo@gmail.com>
2024-06-26 20:24:23 -0500

    Feature/filter category by name (#1)
    * Agregando metodo getByName
    
    * Creando controlador para el endpoint de getByName
    
    * Agregando ruta para el metodo run del controlador getByName
    
    * Eliminando codigo comentado

--- application/models/Category_model.php
+++ application/models/Category_model.php
@@ -1,6 +1,7 @@
 <?php
 
-class Category_model extends CI_Model{
+class Category_model extends CI_Model
+{
 
     private string $table = 'categories';
 
@@ -9,20 +10,35 @@ class Category_model extends CI_Model{
         $this->load->database();
     }
 
-    public function searchById(int $id){
+    public function searchById(int $id)
+    {
         $query = $this->db->get_where($this->table, ['id' => $id]);
         return $query->result();
     }
 
-    public function getAll(){
+    public function getAll()
+    {
 
         $query = $this->db->get($this->table);
         return $query->result();
-
     }
 
-    public function store(array $data){
+    /**
+     * Esta funcion obtiene las categorias por nombres
+     * 
+     * @param string $name Nombre de la category
+     * @return array Resultado de las categorias segun el nombre
+     */
+    public function getByName($name)
+    {
+        $query = $this->db->get_where($this->table, ['title' => $name]);
+        return $query->result();
+    }
+    //hotfix
+    public function store(array $data)
+    {
 
+        //TODO Metodo privado para el set del dataSed
         $dataSet = [
             'title' =>       $data['title'],
             'description' => $data['description']
@@ -38,13 +54,12 @@ class Category_model extends CI_Model{
             'description' => $data['description']
         ];
 
-        return $this->db->where('id', $id)->update('categories', $dataSet);
+        return $this->db->where('id', $id)->update($this->table, $dataSet);
     }
 
-    public function deleteById(int $id){
+    public function deleteById(int $id)
+    {
 
-       return $this->db->delete($this->table, ['id' => $id]);
-        
+        return $this->db->delete($this->table, ['id' => $id]);
     }
-
 }
